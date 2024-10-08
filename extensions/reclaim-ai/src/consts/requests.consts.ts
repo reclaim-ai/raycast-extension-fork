d20c195cf6d04d9cd3d1538b42f9ba300a001871
Sanford Gifford <sanford.r.gifford@gmail.com>
2024-10-08 10:40:21 -0400

    get dotenv working

--- extensions/reclaim-ai/src/consts/requests.consts.ts
+++ extensions/reclaim-ai/src/consts/requests.consts.ts
@@ -1,4 +1,12 @@
+import { config } from "dotenv";
+import path from "path";
+
+// .env files need to be in ~/.config/raycast/extensions/reclaim-ai - .env can't find them there by default.
+(config({ path: path.resolve(__dirname, ".env") }));
+
+const HEADER_KEY_PREFIX = "RAI_REQUEST_HEADER_";
+
 export const ENV_REQUEST_HEADERS: Record<string, string> = Object.entries(process.env).reduce((headers, [key, val]) => {
-  if (val && key.startsWith("RAI_REQUEST_HEADER_")) headers[key] = val;
+  if (val && key.startsWith(HEADER_KEY_PREFIX)) headers[key.slice(HEADER_KEY_PREFIX.length)] = val;
   return headers;
 }, {} as Record<string, string>);
