366517fe87dcb505b4532335b82e731ba9afc7eb
Sanford Gifford <sanford.r.gifford@gmail.com>
2024-10-08 14:46:38 -0400

    debounce useMoment to 1 minute before every 5 minutes

--- extensions/reclaim-ai/src/hooks/useMoment.tsx
+++ extensions/reclaim-ai/src/hooks/useMoment.tsx
@@ -1,13 +1,80 @@
-import useApi from "./useApi";
+import { RefObject, useEffect, useState } from "react";
+import { fetcher } from "../utils/fetcher";
+import { useCallbackSafeRef } from "./useCallbackSafeRef";
 import { ApiResponseMoment } from "./useEvent.types";
 
-export const useMoment = () => {
-  const { data: momentData, error, isLoading } = useApi<ApiResponseMoment>("/moment/next");
+const makeSyncSharedStateHook = <T,>(defaultState: T) => {
+  const lastStateRef = { current: defaultState };
+  return (): [state: T, setState: (state: T) => unknown, stateRef: RefObject<T>] => {
+    const [state, setState] = useState(lastStateRef.current);
+    return [
+      state,
+      useCallbackSafeRef((state) => {
+        lastStateRef.current = state;
+        setState(state);
+      }),
+      lastStateRef,
+    ];
+  };
+};
 
-  if (error) console.error("Error while fetching Moment Next", error);
+const MINUTES_EARLY = 1;
+const FIVE_MINUTE_CHUNKS = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+let initialized = false;
+
+const useMomentData = makeSyncSharedStateHook<ApiResponseMoment | undefined>(undefined);
+const useIsLoading = makeSyncSharedStateHook(true);
+const useError = makeSyncSharedStateHook<Error | undefined>(undefined);
+
+const getNextUpdateEpoch = (now: Date = new Date()) => {
+  // nfmc: next five minute chunk
+  const nfmcDate = new Date(now);
+  // zero-out units below minute
+  nfmcDate.setMilliseconds(0);
+  nfmcDate.setSeconds(0);
+  // get current minute
+  const currentMinute = nfmcDate.getMinutes();
+  // find the first five-minute-chunk which is greater-than or equal-to the current minute
+  const nfmc = FIVE_MINUTE_CHUNKS.find((chunkMinute) => chunkMinute > currentMinute);
+  // set the dates minutes to the next five-minute-chunk.
+  // nfmc should never not be found in `FIVE_MINUTE_CHUNKS` but as a type guard and juuuust
+  // in case fallback to 60 to push to the next hour
+  nfmcDate.setMinutes(nfmc || 60);
+  return nfmcDate.getTime() - MINUTES_EARLY * 60 * 1000;
+};
+
+export const useMoment = () => {
+  const [momentData, setMomentData] = useMomentData();
+  const [isLoading, setIsLoading, isLoadingRef] = useIsLoading();
+  const [error, setError] = useError();
+
+  const checkForUpdate = useCallbackSafeRef(async () => {
+    if (isLoadingRef.current && initialized) return;
+    const now = new Date();
+    const nowEpoch = now.getTime();
+
+    if (nowEpoch > getNextUpdateEpoch(now) || !initialized) {
+      setIsLoading(true);
+      try {
+        const data = await fetcher<ApiResponseMoment>("/moment/next");
+        initialized = true;
+        setIsLoading(false);
+        setMomentData(data);
+      } catch (e) {
+        setError(e instanceof Error ? e : new Error("Problem fetching moment", { cause: e }));
+      }
+    }
+
+    setTimeout(() => checkForUpdate(), getNextUpdateEpoch() - nowEpoch);
+  });
+
+  useEffect(() => {
+    checkForUpdate();
+  }, []);
 
   return {
     momentData,
     isLoading,
+    error,
   };
 };
